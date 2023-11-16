c7de4a1e989e805aab12b2e96e5ce39d788a0dbe
Sam Elfring <elfringsam@gmail.com>
2023-11-16 14:44:26 +0100

    Added timer

---
+++ static/timer.js
@@ -0,0 +1,22 @@
+const taEl = document.getElementsByClassName("typing-area")[0];
+const timerEl = document.getElementById("timer-span");
+
+let time = 60;
+let interval = null;
+
+window.onload = () => {
+    taEl.addEventListener("input", () => {
+        if (interval == null) {
+            interval = setInterval(() => {
+                time -= 0.01;
+                timerEl.innerText = time.toFixed(2);
+
+                if (time <= 0) {
+                    timerEl.innerText = "0.00";
+                    alert("Time is up!");
+                    clearInterval(interval);
+                }
+            }, 10);
+        }
+    });
+}
