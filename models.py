ac29876c537b3d7294ea88228946409cd4c0dd23
LucasHuls <prive@lucashuls.nl>
2023-11-16 10:54:19 +0100

    Added SQLalchemy models

--- models.py
+++ models.py
@@ -1,3 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+
+class Sentences():
+    id = db.Column(db.Integer, primary_key=True)
+    sentence =(db.Column(db.String(100)))
+
+class Statistics():
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20))
+    words_entered = db.Column(db.Integer)
+    word_per_minute = db.Column(db.Integer)
+    time = db.Column(db.DateTime)
