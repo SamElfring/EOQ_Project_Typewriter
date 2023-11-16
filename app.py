ce823c700a15bcdddea1191fd734ae2e93bbdd24
Sam Elfring <elfringsam@gmail.com>
2023-11-16 10:30:01 +0100

    Added Postgresqlite

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
-# Test push -Jip
 from flask import Flask
+from uuid import uuid4
+from models import db
+import postgresqlite
 
 app = Flask(__name__)
+
+# Flask config
+app.config["SECRET_KEY"] = uuid4().hex
+
+# Database configuration
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = postgresqlite.get_uri()
+
+db.init_app(app)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
-
+# Start app
 if __name__ == "__main__":
     app.run()
-
-
-#Test commit
