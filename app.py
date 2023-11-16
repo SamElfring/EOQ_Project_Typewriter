8c0bf969a813aedaafc01340693e74fc1420a8b2
Sam Elfring <elfringsam@gmail.com>
2023-11-16 11:37:26 +0100

    Get random sentence from db

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from uuid import uuid4
 from models import db, Sentences
 import postgresqlite
+import random
 
 app = Flask(__name__)
 
@@ -16,16 +17,32 @@ db.init_app(app)
 
 @app.before_request
 def populate_db():
-    if Sentences.query.first() == None:
+    if Sentences.query.first() is None:
         with open('sentences.txt') as sentences:
             for line in sentences:
-                new_line = Sentences(sentence = line)
+                new_line = Sentences(sentence=line) # type: ignore
                 db.session.add(new_line)
                 db.session.commit()
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/new-sentence")
+def get_new_sentence():
+    """Returns a random sentence from the DB.
+
+    Returns:
+        JSON: JSON object that contains the sentence as a string.
+    """
+    amount_of_sentences = Sentences.query.count()
+    sentence_id = random.randint(1, amount_of_sentences)
+
+    sentence: Sentences = Sentences.query.filter_by(id=sentence_id).first_or_404()
+
+    return {
+        "sentence": sentence.sentence
+    }
 
 # Create tables and models
 with app.app_context():
--- models.py
+++ models.py
@@ -2,10 +2,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-
 class Sentences(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    sentence =(db.Column(db.String(100)))
+    sentence = (db.Column(db.String(100)))
 
 class Leaderboard(db.Model):
     id = db.Column(db.Integer, primary_key=True)
