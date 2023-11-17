98940bb506880b1fee66292f7811c3aeaa2f0f09
Fizitzfux <66255957+fizitzfux@users.noreply.github.com>
2023-11-17 10:53:05 +0000

    Adjust volume of sound effects

--- static/effect.js
+++ static/effect.js
@@ -1,6 +1,7 @@
-function effect(char) {
+function effect(char, volume=1.0) {
     if (!" \n".includes(char)) {
         var src = new Audio(`static/media/${char}.wav`)
+        src.volume = volume
         src.play()
     }
 }
